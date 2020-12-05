37d2934a149d991dc254c23bd8835f3159c8c9c0
Ali Elkoca <>
2020-12-05 14:28:40 +0300

    initial commit

---
+++ MVVMGoodWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMGoodWeather
+//
+//  Created by Ali Elkoca on 5.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
