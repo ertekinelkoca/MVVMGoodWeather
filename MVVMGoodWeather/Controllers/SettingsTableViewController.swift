441f5948c00dddd69a593768b85bde2799e9b220
Ali Elkoca <>
2020-12-08 02:10:01 +0300

    unit values passed to settingstableviewcontroller and binded to the view

--- MVVMGoodWeather/Controllers/SettingsTableViewController.swift
+++ MVVMGoodWeather/Controllers/SettingsTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
     
+    private var settingsViewModel = SettingsViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +19,24 @@ class SettingsTableViewController: UITableViewController {
         
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.settingsViewModel.units.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let settingsItem = self.settingsViewModel.units[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
+        
+        cell.textLabel?.text = settingsItem.displayName
+        
+        return cell
+    }
     
     
     
